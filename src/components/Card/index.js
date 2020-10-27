8544dd0ed19b368018d17320f5079a9e78f2dd70
Kevin <millhousekevin@gmail.com>
2020-10-27 19:29:42 -0400

    first

---
+++ src/components/Card/index.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./style.css";
+
+function Card(props) {
+  //add state.results 
+  // on click for headings 
+  // sort by something simple like dob, or name
+  // 
+  const searchedEmployee = props.results.filter(employee => {
+    let fullname = employee.name.first + " " + employee.name.last
+    return fullname.toUpperCase().includes(props.search.toUpperCase())});
+  return (
+
+    <table className="list-group">
+      {searchedEmployee.map(result => (
+        //use cols 
+        <tr className="list-group-item" key={result.id}>
+          <tr>
+          <th>image</th>
+          <th>name</th>
+          <th>phone</th>
+          <th>email</th>
+          <th>DOB</th>
+          </tr>
+          <tr>
+          <td><img alt={result.name} className="img-fluid" src={result.picture.thumbnail} /></td>
+          <td>{result.name.first}</td><td>{result.name.last}</td>
+          <td>{result.phone}</td>
+          <td>{result.email}</td>
+          <td>{result.dob.date.substring(0,10)}</td>
+          </tr>
+        </tr>
+      ))}
+    </table>
+  );
+}
+
+export default Card;
---
+++ src/pages/Discover.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Card from "../components/Card";
+
+
+
+class Discover extends Component {
+  state = {
+    results:[],
+    search:""
+  };
+
+  componentDidMount() {
+    this.loadsEmployee();
+  }
+
+  loadsEmployee = () => {
+    API.getUsers()
+      .then(res => this.setState({results: res.data.results}))
+      .catch(err => console.log(err));
+  };
+  
+  handleInputChange = event => {
+    this.setState({ search: event.target.value });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Employee Directory</h1>
+        <h3 className="text-center">
+          Click on carrots to filter by heading or use the search to narrow your!
+        </h3>
+        <form className="search">
+      <div className="form-group">
+        <label htmlFor="employee">Employee Name:</label>
+        <input
+          value={this.state.search}
+          onChange={this.handleInputChange}
+          type="text"
+          className="form-control"
+          placeholder="Search for employees by..."
+          id="breed"
+        />
+      </div>
+    </form>
+        <Card results={this.state.results} search={this.state.search}/>
+      </div>
+    );
+  }
+}
+
+export default Discover;
