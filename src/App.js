7ef802f8187d28898e612f52ed04d43712dc9c20
Kevin <millhousekevin@gmail.com>
2020-10-27 21:07:42 -0400

    second

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Discover from "./pages/Discover";
-// import Search from "./pages/Search";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
@@ -10,8 +9,7 @@ function App() {
     <Router>
       <div>
         <Wrapper>
-        <Route exact path="/" component={Discover} />
-          {/* <Route exact path="/search" component={Search} /> */}
+        <Route exact path="/employeeDirectory3" component={Discover} />
         </Wrapper>
         <Footer />
       </div>
