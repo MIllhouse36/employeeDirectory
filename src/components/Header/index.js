b1fe0760d9df49aa249add670de48ded55146ee2
Kevin <millhousekevin@gmail.com>
2021-06-27 13:36:08 -0400

    formatting and clean up

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Header = ()=> {
+const Header = () => {
   return (
     <div>
-  <h1 className="text-center">Employee Directory</h1>
-        <h3 className="text-center">
-          Click on carrots to filter by heading or use the search to narrow your!
-        </h3>
+      <h1 className="text-center">Employee Directory</h1>
+      <h3 className="text-center">
+        Click on carrots to filter by heading or use the search to narrow your!
+      </h3>
     </div>
   );
 };
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,39 +4,41 @@ import Table from "../Table";
 
 class Discover extends Component {
   state = {
-    results:[],
-    search:""
+    results: [],
+    search: "",
   };
 
   componentDidMount = () => {
-    API.getUsers().then(res => 
-      this.setState({
-        results: res.data.results}))
-      .catch(err => console.log(err));
+    API.getUsers()
+      .then((res) =>
+        this.setState({
+          results: res.data.results,
+        })
+      )
+      .catch((err) => console.log(err));
   };
-  
-  handleInputChange = event => {
+
+  handleInputChange = (event) => {
     this.setState({ search: event.target.value });
   };
 
   render() {
     return (
       <div>
-      
         <form className="search">
-      <div className="form-group">
-        <label htmlFor="employee">Employee Name:</label>
-        <input
-          value={this.state.search}
-          onChange={this.handleInputChange}
-          type="text"
-          className="form-control"
-          placeholder="Search for employees by..."
-          id="breed"
-        />
-      </div>
-    </form>
-        <Table results={this.state.results} search={this.state.search}/>
+          <div className="form-group">
+            <label htmlFor="employee">Employee Name:</label>
+            <input
+              value={this.state.search}
+              onChange={this.handleInputChange}
+              type="text"
+              className="form-control"
+              placeholder="Search for employees by..."
+              id="breed"
+            />
+          </div>
+        </form>
+        <Table results={this.state.results} search={this.state.search} />
       </div>
     );
   }
